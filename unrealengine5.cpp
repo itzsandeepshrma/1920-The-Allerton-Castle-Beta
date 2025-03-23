2646eab0f87ac2b749c5ca47fb95a2603d0cb862
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-03-23 13:04:46 +0530

    Update unrealengine5.cpp

--- unrealengine5.cpp
+++ unrealengine5.cpp
@@ -1,1 +1,21 @@
+#include "unrealengine.h"
+#include "Modules/ModuleManager.h"
+#include "Logging/LogMacros.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogUnrealEngine, Log, All);
+
+class FUnrealEngineGameModule : public FDefaultGameModuleImpl
+{
+public:
+    virtual void StartupModule() override
+    {
+        UE_LOG(LogUnrealEngine, Warning, TEXT("Unreal Engine Module Initialized!"));
+    }
+
+    virtual void ShutdownModule() override
+    {
+        UE_LOG(LogUnrealEngine, Warning, TEXT("Unreal Engine Module Shut Down!"));
+    }
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FUnrealEngineGameModule, unrealengine, "unrealengine");
