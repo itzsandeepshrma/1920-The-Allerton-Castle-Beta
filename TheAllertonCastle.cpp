3a61744205412085443455ff7952b6b0df4f4fad
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-03-23 13:05:19 +0530

    Update TheAllertonCastle.cpp

--- TheAllertonCastle.cpp
+++ TheAllertonCastle.cpp
@@ -1,1 +1,21 @@
+#include "TheAllertonCastle.h"
+#include "Modules/ModuleManager.h"
+#include "Logging/LogMacros.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogAllertonCastle, Log, All);
+
+class FTheAllertonCastleGameModule : public FDefaultGameModuleImpl
+{
+public:
+    virtual void StartupModule() override
+    {
+        UE_LOG(LogAllertonCastle, Warning, TEXT("The Allerton Castle Module Initialized!"));
+    }
+
+    virtual void ShutdownModule() override
+    {
+        UE_LOG(LogAllertonCastle, Warning, TEXT("The Allerton Castle Module Shut Down!"));
+    }
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FTheAllertonCastleGameModule, TheAllertonCastle, "TheAllertonCastle");
